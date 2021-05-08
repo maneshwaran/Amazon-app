b80c4ae2df4ee1cf6ce346ebec0ba1ec820408ab
maneshwaran <manu.ariyalur@gmail.com>
2021-05-08 09:24:32 +0530

    Added Rating,Product,HomeScreen,ProductScreen Components and styles

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,67 +4,73 @@ const data = {
             _id: '1',
             name: 'Nike Slim Shirt',
             category: 'Shirts',
-            image: './images/p1.jpg',
+            image: '/images/p1.jpg',
             price: 120,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
         {
             _id: '2',
             name: 'Adidas Fit Shirt',
             category: 'Shirts',
-            image: './images/p2.jpg',
+            image: '/images/p2.jpg',
             price: 100,
             brand: 'Adidas',
             rating: 4,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
         {
             _id: '3',
             name: 'Lacoste Free Shirt',
             category: 'Shirts',
-            image: './images/p3.jpg',
+            image: '/images/p3.jpg',
             price: 220,
             brand: 'Lacoste',
             rating: 4.8,
             numReviews: 17,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
         {
             _id: '4',
             name: 'Nike Slim Pant',
             category: 'Pants',
-            image: './images/p4.jpg',
+            image: '/images/p4.jpg',
             price: 78,
             brand: 'Nike',
             rating: 4.5,
             numReviews: 14,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
         {
             _id: '5',
             name: 'Puma Slim Pant',
             category: 'Pants',
-            image: './images/p5.jpg',
+            image: '/images/p5.jpg',
             price: 65,
             brand: 'Puma',
             rating: 4.5,
             numReviews: 10,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
         {
             _id: '6',
             name: 'Addidas Fit Shirt',
             category: 'Pants',
-            image: './images/p6.jpg',
+            image: '/images/p6.jpg',
             price: 139,
             brand: 'Addidas',
             rating: 4.5,
             numReviews: 15,
-            description: 'high quality product'
+            description: 'high quality product',
+            countInStock: 10
         },
     ],
 };
