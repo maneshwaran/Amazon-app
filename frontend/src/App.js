95f5d5c06347fa3b455fa1c12daf8463eb798c4b
maneshwaran <manu.ariyalur@gmail.com>
2021-05-09 19:47:32 +0530

    Added CartScreen and AddToCartActions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import ProductScreen from './screens/ProductScreen';
 import HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const {cartItems} = cart;
+
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="/">Amazon</a>
+            <Link className="brand" to="/">Amazon</Link>
           </div>
           <div>
-            <a href="/cart">Cart</a>
-            <a href="/signin">Sign In</a>
+            <Link to="/cart">Cart
+            {cartItems.length > 0 && (
+              <span className="badge">{cartItems.length}</span>
+            )}</Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
           <Route path='/' component={HomeScreen} exact></Route>
           
