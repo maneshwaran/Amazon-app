1a22c24c7a0a7324af32a1018c5caf7d922ed9f5
maneshwaran <manu.ariyalur@gmail.com>
2021-05-09 18:49:15 +0530

    Added Actions,Reducer for Product screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import thunk from 'redux-thunk';
 import {applyMiddleware, combineReducers, compose, createStore} from 'redux';
-import { productListReducer } from "./reducers/productReducers";
+import { productDetailsReducer, productListReducer } from "./reducers/productReducers";
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 })
 const composeEnhancer = window.__REDUX_DEVTOOLD_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
